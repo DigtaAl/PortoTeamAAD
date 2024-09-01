6b2b98a67f77c3d71510e95b0de4e3b9c71b7275
Rifky Andigta Al-Fathir <digtaalfathir36@gmail.com>
2024-09-01 23:38:04 +0700

    Update Achivement page

---
+++ src/components/Timeline.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="container">
+    <div class="timeline">
+      <ul>
+        <li>
+          <div class="timeline-content">
+            <h3 class="date">1 - 6th july, 2024</h3>
+            <h1>KRI 2024 ( Nasional )</h1>
+            <p>Acara ini diadakan secara offline di Universitas Muhammadiyah Surakarta, dan tim berhasil meraih juara 2 tingkat nasional dalam kompetisi tersebut. Informasi lebih lanjut dapat dilihat di sini.</p>
+            <img src="./../assets/images/AAD.jpg" alt="KRI 2024 (Nasional)" />
+          </div>
+        </li>
+        <li>
+          <div class="timeline-content">
+            <h3 class="date">28th may - 1st June, 2024</h3>
+            <h1>KRI 2024 ( Regional )</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur tempora ab laudantium voluptatibus aut eos placeat laborum, quibusdam exercitationem labore.</p>
+            <img src="./../assets/images/AAD.jpg" alt="KRI 2024 (Regional)" />
+          </div>
+        </li>
+        <li>
+          <div class="timeline-content">
+            <h3 class="date">22 - 25th june, 2023</h3>
+            <h1>KRI 2023 ( Nasional )</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur tempora ab laudantium voluptatibus aut eos placeat laborum, quibusdam exercitationem labore.</p>
+            <img src="./../assets/images/AAD.jpg" alt="KRI 2023 (Nasional)" />
+          </div>
+        </li>
+        <li>
+          <div class="timeline-content">
+            <h3 class="date">28 - 31 may, 2023</h3>
+            <h1>KRI 2023 ( Regional )</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur tempora ab laudantium voluptatibus aut eos placeat laborum, quibusdam exercitationem labore.</p>
+            <img src="./../assets/images/AAD.jpg" alt="KRI 2023 (Regional)" />
+          </div>
+        </li>
+        <li>
+          <div class="timeline-content">
+            <h3 class="date">3 - 8th july, 2024</h3>
+            <h1>Heading 5</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur tempora ab laudantium voluptatibus aut eos placeat laborum, quibusdam exercitationem labore.</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Timeline",
+};
+</script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 100px 0;
+  font-family: "Montserrat";
+  background-color: #131313;
+}
+.timeline {
+  width: 80%;
+  height: auto;
+  max-width: 800px;
+  margin: 0 auto;
+  position: relative;
+}
+
+.timeline ul {
+  list-style: none;
+}
+.timeline ul li {
+  padding: 20px;
+  background-color: #1e1f22;
+  color: white;
+  border-radius: 10px;
+  margin-bottom: 20px;
+}
+.timeline ul li:last-child {
+  margin-bottom: 0;
+}
+.timeline-content h1 {
+  font-weight: 500;
+  font-size: 25px;
+  line-height: 30px;
+  margin-bottom: 10px;
+}
+.timeline-content p {
+  font-size: 16px;
+  line-height: 30px;
+  font-weight: 300;
+}
+.timeline-content .date {
+  font-size: 12px;
+  font-weight: 300;
+  margin-bottom: 10px;
+  letter-spacing: 2px;
+}
+.timeline-content img {
+  width: 100%;
+  height: auto;
+  border-radius: 10px;
+  margin-bottom: 15px;
+  object-fit: cover;
+}
+@media only screen and (min-width: 768px) {
+  .timeline:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 2px;
+    height: 100%;
+    background-color: gray;
+  }
+  .timeline ul li {
+    width: 50%;
+    position: relative;
+    margin-bottom: 50px;
+  }
+  .timeline ul li:nth-child(odd) {
+    float: left;
+    clear: right;
+    transform: translateX(-30px);
+    border-radius: 20px 0px 20px 20px;
+  }
+  .timeline ul li:nth-child(even) {
+    float: right;
+    clear: left;
+    transform: translateX(30px);
+    border-radius: 0px 20px 20px 20px;
+  }
+  .timeline ul li::before {
+    content: "";
+    position: absolute;
+    height: 20px;
+    width: 20px;
+    border-radius: 50%;
+    background-color: gray;
+    top: 0px;
+  }
+  .timeline ul li:nth-child(odd)::before {
+    transform: translate(50%, -50%);
+    right: -30px;
+  }
+  .timeline ul li:nth-child(even)::before {
+    transform: translate(-50%, -50%);
+    left: -30px;
+  }
+  .timeline-content .date {
+    position: absolute;
+    top: -30px;
+  }
+  .timeline ul li:hover::before {
+    background-color: aqua;
+  }
+}
+</style>
--- src/views/Achievement.vue
+++ src/views/Achievement.vue
@@ -1,16 +1,30 @@
 <template>
-  <div class="min-h-svh max-w-[1440px] mx-auto">
-    <HeaderNav />
-  </div>
+  <HeaderNav />
+  <Timeline />
+  <footer class="footer-section">
+    <div class="copyright-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-6 col-lg-6 text-center text-lg-left">
+            <div class="copyright-text">
+              <p>Copyright &copy; 2024, Made with 🤍 by <a href="https://github.com/DigtaAl">Rifky Andigta Al-Fathir</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
 import HeaderNav from "@/components/Header.vue";
+import Timeline from "@/components/Timeline.vue";
 
 export default {
   name: "Achievement",
   components: {
     HeaderNav,
+    Timeline,
   },
 };
 </script>
